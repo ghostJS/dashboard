567937e43f4f3ad57459cdea1995e72cde67a9bd
ghostJS <ghostjs@o2.pl>
2016-06-17 20:46:43 +0200

    SPA - routing added

--- web/js/app.js
+++ web/js/app.js
@@ -1,10 +1,30 @@
 (function(){
-var app = angular.module('dashboard', []);
+	// dodanie zależności do modułu - ngRoute
+	var app = angular.module('dashboard', ['ngRoute']);
 
-app.controller('globalController', ['$scope', function($scope){
-	$scope.message = "Wiadomość z kontrolera";
-	$scope.wskaz = "Wskaźniki"
-}]);
+
+	// odwolanie do modułu i do metody konfig, dzięki zależności routeProvider można definiować ścieżki
+	app.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/dashboard', {
+				templateUrl: 'views/dashboard.html'
+			})
+			.when('/employees', {
+				templateUrl: 'views/employees.html'
+			})
+			.when('/clients', {
+				templateUrl: 'views/clients.html'
+			})
+			.otherwise({
+				redirectTo: '/dashboard'
+			});
+	}]);
+
+	app.controller('globalController', ['$scope', function($scope){
+		$scope.message = "Wiadomość z kontrolera";
+		$scope.wskaz = "Wskaźniki"
+	}]);
 
 })();
+
 
